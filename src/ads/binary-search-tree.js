28f09e909696d0d6066845f175e6eac40ae8fe57
Andrei Domsa <domsa.andrei.i@gmail.com>
2017-03-16 17:00:51 +0200

    code formatting

--- src/ads/binary-search-tree.js
+++ src/ads/binary-search-tree.js
@@ -3,207 +3,216 @@
 'use strict';
 
 /**
- *  Binary Search Tree Data structure
+ * Binary Search Tree Data structure
  */
 var Bst = Bst || {};
 
 Bst.Node = function(k, data) {
-  this.data = data; // any
-  
-  this.key = k; // integer
-  this.parent = null; // Node
-  this.left = null; // Node
-  this.right = null; // Node
+	this.data = data; // any
+
+	this.key = k; // integer
+	this.parent = null; // Node
+	this.left = null; // Node
+	this.right = null; // Node
 };
 
 Bst.Tree = function() {
-  
-  var self = this;
-  
-  /* public methods */
-  self.search = searchNode;
-  self.minimum = minimumNode;
-  self.maximum = maximumNode;
-  self.successor = successor;
-  self.predecessor = predecessor;
-  self.insert = insert;
-  self.remove = remove;
-  self.inorder = inorder;
-  self.displayInorder = displayInorder;
-  self.displayTree = displayTree;
-  
-  /* private vars */
-  var root = null; // root node
-  
-  /**
-   * Search a node by key
-   */
-  function searchNode(key) {
-    return search(root, key);
-  }
-  
-  /**
-   * Recursive search a node by key
-   */
-  function search(node, key) {
-    if (node === null || node.key == key)
-      return node;
 
-    return search((node.key > key) ? node.left : node.right, key);
-  }
+	var self = this;
 
-  /**
-   * Retrieve the tree node with the minimum key
-   */
-  function minimumNode() {
-    return minimum(root);
-  }
-  
-  /**
-   * Return the leftmost child of a node
-   */
-  function minimum(node) {
-    var n = node;
-    while (n.left !== null) n = n.left;
-    return n;
-  }
+	/* public methods */
+	self.search = searchNode;
+	self.minimum = minimumNode;
+	self.maximum = maximumNode;
+	self.successor = successor;
+	self.predecessor = predecessor;
+	self.insert = insert;
+	self.remove = remove;
+	self.inorder = inorder;
+	self.displayInorder = displayInorder;
+	self.displayTree = displayTree;
 
-  /**
-   * Retrieve the tree node with the maximum key
-   */
-  function maximumNode() {
-    return maximum(root);
-  }
-  
-  /**
-   * Return the rightmost child of a node
-   */
-  function maximum(node) {
-    var n = node;
-    while (n.right !== null) n = n.right;
-    return n;
-  }
+	/* private vars */
+	var root = null; // root node
 
-  /**
-   * Retrieve the successor of a given node by its key
-   */
-  function successor(node) {
-    if (node.right !== null) return minimum(node.right);
+	/**
+	 * Search a node by key
+	 */
+	function searchNode(key) {
+		return search(root, key);
+	}
 
-    if (node.parent === null) return null;
+	/**
+	 * Recursive search a node by key
+	 */
+	function search(node, key) {
+		if (node === null || node.key == key)
+			return node;
 
-    var s = node.parent;
-    while (s !== null && node == s.right) {
-      node = s;
-      s = s.parent;
-    }
-    return s;
-  }
+		return search((node.key > key) ? node.left : node.right, key);
+	}
 
-  /**
-   * Retrieve the predecessor of a given node by its key
-   */
-  function predecessor(node) {
-    if (node.left !== null) return maximum(node.left);
+	/**
+	 * Retrieve the tree node with the minimum key
+	 */
+	function minimumNode() {
+		return minimum(root);
+	}
 
-    if (node.parent === null) return null;
+	/**
+	 * Return the leftmost child of a node
+	 */
+	function minimum(node) {
+		var n = node;
+		while (n.left !== null)
+			n = n.left;
+		return n;
+	}
 
-    var p = node.parent;
-    while (p !== null && node == p.left) {
-      node = p;
-      p = p.parent;
-    }
-    return p;
-  }
+	/**
+	 * Retrieve the tree node with the maximum key
+	 */
+	function maximumNode() {
+		return maximum(root);
+	}
 
-  /**
-   * Insert a new node
-   */
-  function insert(node) {
-    var y = null;
-    var r = root;
-    while (r !== null) {
-      y = r;
-      r = (node.key < r.key) ? r.left : r.right;
-    }
-    node.parent = y;
-    if (y === null)
-      root = node;
-    else if (node.key < y.key)
-      y.left = node;
-    else
-      y.right = node;
-  }
+	/**
+	 * Return the rightmost child of a node
+	 */
+	function maximum(node) {
+		var n = node;
+		while (n.right !== null)
+			n = n.right;
+		return n;
+	}
 
-  /**
-   * Remove a node
-   */
-  function remove(node) {
-    if (node === null) return null;
+	/**
+	 * Retrieve the successor of a given node by its key
+	 */
+	function successor(node) {
+		if (node.right !== null)
+			return minimum(node.right);
 
-    var ny = (node.left === null || node.right === null) ? node : successor(node);
-    var nx = (ny.left !== null) ? ny.left : ny.right;
+		if (node.parent === null)
+			return null;
 
-    if (nx !== null) {
-      nx.parent = ny.parent;
-    }
+		var s = node.parent;
+		while (s !== null && node == s.right) {
+			node = s;
+			s = s.parent;
+		}
+		return s;
+	}
 
-    if (ny.parent === null) {
-      root = nx;
-    } else {
-      if (ny == ny.parent.left) {
-        ny.parent.left = nx;
-      } else {
-        ny.parent.right = nx;
-      }
-    }
+	/**
+	 * Retrieve the predecessor of a given node by its key
+	 */
+	function predecessor(node) {
+		if (node.left !== null)
+			return maximum(node.left);
 
-    if (ny != node) {
-      node.key = ny.key;
-      node.data = ny.data;
-    }
+		if (node.parent === null)
+			return null;
 
-    return ny;
-  }
-  
-  /**
-   * Executes a provided function for each tree element, sorted by key
-   */
-  function inorder(node, callback) {
-    if (node !== null) {
-      inorder(node.left, callback);
-      if (typeof callback === 'function') callback(node);
-      inorder(node.right, callback);
-    }
-  }
+		var p = node.parent;
+		while (p !== null && node == p.left) {
+			node = p;
+			p = p.parent;
+		}
+		return p;
+	}
 
-  /**
-   * Display the sorted keys of the tree
-   */
-  function displayInorder() {
-	var s = [];
-    inorder(root, function(node){
-    	s.push(node.key);
-    });
-    console.info(s.join(', '));
-  }
+	/**
+	 * Insert a new node
+	 */
+	function insert(node) {
+		var y = null;
+		var r = root;
+		while (r !== null) {
+			y = r;
+			r = (node.key < r.key) ? r.left : r.right;
+		}
+		node.parent = y;
+		if (y === null)
+			root = node;
+		else if (node.key < y.key)
+			y.left = node;
+		else
+			y.right = node;
+	}
 
-  /**
-   * Display the tree structure with indentation
-   */
-  function displayTree() {
-    display(root, 0);
-  }
-  
-  /**
-   * 
-   */
-  function display(node, indent) {
-    if (node !== null) {
-      display(node.right, indent + 2);
-      console.info(" ".repeat(indent), node.key);
-      display(node.left, indent + 2);
-    }
-  }
-  
+	/**
+	 * Remove a node
+	 */
+	function remove(node) {
+		if (node === null)
+			return null;
+
+		var ny = (node.left === null || node.right === null) ? node
+				: successor(node);
+		var nx = (ny.left !== null) ? ny.left : ny.right;
+
+		if (nx !== null) {
+			nx.parent = ny.parent;
+		}
+
+		if (ny.parent === null) {
+			root = nx;
+		} else {
+			if (ny == ny.parent.left) {
+				ny.parent.left = nx;
+			} else {
+				ny.parent.right = nx;
+			}
+		}
+
+		if (ny != node) {
+			node.key = ny.key;
+			node.data = ny.data;
+		}
+
+		return ny;
+	}
+
+	/**
+	 * Executes a provided function for each tree element, sorted by key
+	 */
+	function inorder(node, callback) {
+		if (node !== null) {
+			inorder(node.left, callback);
+			if (typeof callback === 'function')
+				callback(node);
+			inorder(node.right, callback);
+		}
+	}
+
+	/**
+	 * Display the sorted keys of the tree
+	 */
+	function displayInorder() {
+		var s = [];
+		inorder(root, function(node) {
+			s.push(node.key);
+		});
+		console.info(s.join(', '));
+	}
+
+	/**
+	 * Display the tree structure with indentation
+	 */
+	function displayTree() {
+		display(root, 0);
+	}
+
+	/**
+	 * Recursive display
+	 */
+	function display(node, indent) {
+		if (node !== null) {
+			display(node.right, indent + 2);
+			console.info(" ".repeat(indent), node.key);
+			display(node.left, indent + 2);
+		}
+	}
+
 };
--- src/ads/red-black-tree.js
+++ src/ads/red-black-tree.js
@@ -3,316 +3,380 @@
 'use strict';
 
 /**
- *	Red-Black Trees
+ * Red-Black Trees Data structure
  */
-
 var Rbt = Rbt || {};
 
 Rbt.Color = {
-  RED: 'R',
-  BLACK: 'B'
+	RED : 'R',
+	BLACK : 'B'
 };
 
 Rbt.Node = function(k, data) {
-  this.data = data; // any
-  
-  this.key = k; // integer
-  this.parent = (k === 0) ? this : Rbt.NULL; // Node
-  this.left = (k === 0) ? this : Rbt.NULL; // Node
-  this.right = (k === 0) ? this : Rbt.NULL; // Node
-  this.color = Rbt.Color.BLACK; // Rbt.Color.RED | Rbt.Color.BLACK
+	this.data = data; // any
+
+	this.key = k; // integer
+	this.parent = (k === 0) ? this : Rbt.NULL; // Node
+	this.left = (k === 0) ? this : Rbt.NULL; // Node
+	this.right = (k === 0) ? this : Rbt.NULL; // Node
+	this.color = Rbt.Color.BLACK; // Rbt.Color.RED | Rbt.Color.BLACK
 };
 
 Rbt.NULL = new Rbt.Node(0);
 
 Rbt.Tree = function() {
-  
-  var self = this;
-  
-  /* private vars */
-  var root = Rbt.NULL; // root node
 
-  /* public methods */
-  self.search = searchNode;
-  self.minimum = minimumNode;
-  self.maximum = maximumNode;
-  self.successor = successor;
-  self.predecessor = predecessor;
-  self.insert = insert;
-  self.remove = remove;
-  self.inorderNodes = inorderNodes;
-  self.displayTree = displayTree;
-  
-  /* Private methods */
-  
-  function searchNode(k) {
-    return search(root, k);
-  }
+	var self = this;
 
-  function minimumNode() {
-    return minimum(root);
-  }
+	/* public methods */
+	self.search = searchNode;
+	self.minimum = minimumNode;
+	self.maximum = maximumNode;
+	self.successor = successor;
+	self.predecessor = predecessor;
+	self.insert = insert;
+	self.remove = remove;
+	self.inorder = inorder;
+	self.displayInorder = displayInorder;
+	self.inorderNodes = inorderNodes;
+	self.displayTree = displayTree;
 
-  function maximumNode() {
-    return maximum(root);
-  }
+	/* private vars */
+	var root = Rbt.NULL; // root node
 
-  function successor(node) {
-    if (node.right != Rbt.NULL) return minimum(node.right);
+	/**
+	 * Search a node by key
+	 */
+	function searchNode(key) {
+		return search(root, key);
+	}
 
-    if (node.parent == Rbt.NULL) return Rbt.NULL;
+	/**
+	 * Recursive search a node by key
+	 */
+	function search(node, k) {
+		if (node == Rbt.NULL || node.key == k)
+			return node;
 
-    var s = node.parent;
-    while (s != Rbt.NULL && node == s.right) {
-      node = s;
-      s = s.parent;
-    }
-    return s;
-  }
+		return search((node.key > k) ? node.left : node.right, k);
+	}
 
-  function predecessor(node) {
-    if (node.left != Rbt.NULL) return maximum(node.left);
+	/**
+	 * Retrieve the tree node with the minimum key
+	 */
+	function minimumNode() {
+		return minimum(root);
+	}
 
-    if (node.parent == Rbt.NULL) return Rbt.NULL;
+	/**
+	 * Return the leftmost child of a node
+	 */
+	function minimum(n) {
+		var m = n;
+		while (m.left != Rbt.NULL)
+			m = m.left;
+		return m;
+	}
 
-    var s = node.parent;
-    while (s != Rbt.NULL && node == s.left) {
-      node = s;
-      s = s.parent;
-    }
-    return s;
-  }
+	/**
+	 * Retrieve the tree node with the maximum key
+	 */
+	function maximumNode() {
+		return maximum(root);
+	}
 
-  function insert(node) {
-    var y = Rbt.NULL;
-    var r = root;
+	/**
+	 * Return the rightmost child of a node
+	 */
+	function maximum(n) {
+		var m = n;
+		while (m.right != Rbt.NULL)
+			m = m.right;
+		return m;
+	}
 
-    while (r != Rbt.NULL) {
-      y = r;
-      r = (node.key < r.key) ? r.left : r.right;
-    }
+	/**
+	 * Retrieve the successor of a given node by its key
+	 */
+	function successor(node) {
+		if (node.right != Rbt.NULL)
+			return minimum(node.right);
 
-    node.parent = y;
+		if (node.parent == Rbt.NULL)
+			return Rbt.NULL;
 
-    if (y == Rbt.NULL)
-      root = node;
-    else if (node.key < y.key)
-      y.left = node;
-    else
-      y.right = node;
+		var s = node.parent;
+		while (s != Rbt.NULL && node == s.right) {
+			node = s;
+			s = s.parent;
+		}
+		return s;
+	}
 
-    node.left = node.right = Rbt.NULL;
-    node.color = Rbt.Color.RED;
-    
-    insertFixup(node);
-  }
+	/**
+	 * Retrieve the predecessor of a given node by its key
+	 */
+	function predecessor(node) {
+		if (node.left != Rbt.NULL)
+			return maximum(node.left);
 
-  function remove(node) {
-    if (node == Rbt.NULL) return Rbt.NULL;
+		if (node.parent == Rbt.NULL)
+			return Rbt.NULL;
 
-    var ny = (node.left == Rbt.NULL || node.right == Rbt.NULL) ? node : successor(node);
-    var nx = (ny.left != Rbt.NULL) ? ny.left : ny.right;
+		var s = node.parent;
+		while (s != Rbt.NULL && node == s.left) {
+			node = s;
+			s = s.parent;
+		}
+		return s;
+	}
 
-    nx.parent = ny.parent;
+	/**
+	 * Insert a new node
+	 */
+	function insert(node) {
+		var y = Rbt.NULL;
+		var r = root;
 
-    if (ny.parent == Rbt.NULL) {
-      root = nx;
-    } else {
-      if (ny == ny.parent.left) {
-        ny.parent.left = nx;
-      } else {
-        ny.parent.right = nx;
-      }
-    }
+		while (r != Rbt.NULL) {
+			y = r;
+			r = (node.key < r.key) ? r.left : r.right;
+		}
 
-    if (ny != node) {
-      node.key = ny.key;
-      node.data = ny.data;
-    }
+		node.parent = y;
 
-    if (ny.color == Rbt.Color.BLACK)
-      deleteFixup(nx);
+		if (y == Rbt.NULL)
+			root = node;
+		else if (node.key < y.key)
+			y.left = node;
+		else
+			y.right = node;
 
-    return ny;
-  }
+		node.left = node.right = Rbt.NULL;
+		node.color = Rbt.Color.RED;
 
-  function inorderNodes() {
-    var nodes = [];
-    inorder(root, nodes);
-    return nodes;
-  }
+		insertFixup(node);
+	}
 
-  function displayTree() {
-    display(root, 0);
-  }
+	/**
+	 * Remove a node
+	 */
+	function remove(node) {
+		if (node == Rbt.NULL)
+			return Rbt.NULL;
 
-  /** Private methods */
-  function search(node, k) {
-    if (node == Rbt.NULL || node.key == k)
-      return node;
+		var ny = (node.left == Rbt.NULL || node.right == Rbt.NULL) ? node
+				: successor(node);
+		var nx = (ny.left != Rbt.NULL) ? ny.left : ny.right;
 
-    return search((node.key > k) ? node.left : node.right, k);
-  }
+		nx.parent = ny.parent;
 
-  function minimum(n) {
-    var m = n;
-    while (m.left != Rbt.NULL) m = m.left;
-    return m;
-  }
+		if (ny.parent == Rbt.NULL) {
+			root = nx;
+		} else {
+			if (ny == ny.parent.left) {
+				ny.parent.left = nx;
+			} else {
+				ny.parent.right = nx;
+			}
+		}
 
-  function maximum(n) {
-    var m = n;
-    while (m.right != Rbt.NULL) m = m.right;
-    return m;
-  }
+		if (ny != node) {
+			node.key = ny.key;
+			node.data = ny.data;
+		}
 
-  function inorder(node, nodes) {
-    if (node != Rbt.NULL) {
-      inorder(node.left, nodes);
-      nodes.push(node);
-      inorder(node.right, nodes);
-    }
-  }
+		if (ny.color == Rbt.Color.BLACK)
+			deleteFixup(nx);
 
-  function display(node, indent) {
-    if (node != Rbt.NULL) {
-      display(node.right, indent + 2);
-      console.info(" ".repeat(indent), node.key + ":" + node.color);
-      display(node.left, indent + 2);
-    }
-  }
+		return ny;
+	}
 
-  function leftRotate(node) {
-    var ny = node.right;
+	/**
+	 * Executes a provided function for each tree element, sorted by key
+	 */
+	function inorder(node, callback) {
+		if (node !== null) {
+			inorder(node.left, callback);
+			if (typeof callback === 'function')
+				callback(node);
+			inorder(node.right, callback);
+		}
+	}
 
-    node.right = ny.left;
-    if (ny.left != Rbt.NULL)
-      ny.left.parent = node;
+	/**
+	 * Display the sorted keys of the tree
+	 */
+	function displayInorder() {
+		var s = [];
+		inorder(root, function(node) {
+			s.push(node.key);
+		});
+		console.info(s.join(', '));
+	}
 
-    ny.parent = node.parent;
-    if (node.parent == Rbt.NULL)
-      root = ny;
-    else if (node == node.parent.left)
-      node.parent.left = ny;
-    else
-      node.parent.right = ny;
+	/**
+	 * Returns all the nodes of the tree as array, sorted by key
+	 */
+	function inorderNodes() {
+		var nodes = [];
+		inorder(root, function(node) {
+			nodes.push(node);
+		});
+		return nodes;
+	}
 
-    ny.left = node;
-    node.parent = ny;
-  }
+	/**
+	 * Display the tree structure with indentation
+	 */
+	function displayTree() {
+		display(root, 0);
+	}
 
-  function rightRotate(node) {
-    var ny = node.left;
+	/**
+	 * Recursive display
+	 */
+	function display(node, indent) {
+		if (node != Rbt.NULL) {
+			display(node.right, indent + 2);
+			console.info(" ".repeat(indent), node.key + ":" + node.color);
+			display(node.left, indent + 2);
+		}
+	}
 
-    node.left = ny.right;
-    if (ny.right != Rbt.NULL)
-      ny.right.parent = node;
+	function leftRotate(node) {
+		var ny = node.right;
 
-    ny.parent = node.parent;
-    if (node.parent == Rbt.NULL)
-      root = ny;
-    else if (node == node.parent.left)
-      node.parent.left = ny;
-    else
-      node.parent.right = ny;
+		node.right = ny.left;
+		if (ny.left != Rbt.NULL)
+			ny.left.parent = node;
 
-    ny.right = node;
-    node.parent = ny;
-  }
+		ny.parent = node.parent;
+		if (node.parent == Rbt.NULL)
+			root = ny;
+		else if (node == node.parent.left)
+			node.parent.left = ny;
+		else
+			node.parent.right = ny;
 
-  function insertFixup(node) {
-    var y;
-    while (node.parent.color == Rbt.Color.RED) {
-      if (node.parent == node.parent.parent.left) {
-        y = node.parent.parent.right;
-        if (y.color == Rbt.Color.RED) {
-          node.parent.color = Rbt.Color.BLACK;
-          y.color = Rbt.Color.BLACK;
-          node.parent.parent.color = Rbt.Color.RED;
-          node = node.parent.parent;
-        } else {
-          if (node == node.parent.right) {
-            node = node.parent;
-            leftRotate(node);
-          }
-          node.parent.color = Rbt.Color.BLACK;
-          node.parent.parent.color = Rbt.Color.RED;
-          rightRotate(node.parent.parent);
-        }
-      } else {
-        y = node.parent.parent.left;
-        if (y.color == Rbt.Color.RED) {
-          node.parent.color = Rbt.Color.BLACK;
-          y.color = Rbt.Color.BLACK;
-          node.parent.parent.color = Rbt.Color.RED;
-          node = node.parent.parent;
-        } else {
-          if (node == node.parent.left) {
-            node = node.parent;
-            rightRotate(node);
-          }
-          node.parent.color = Rbt.Color.BLACK;
-          node.parent.parent.color = Rbt.Color.RED;
-          leftRotate(node.parent.parent);
-        }
-      }
-    }
-    root.color = Rbt.Color.BLACK;
-  }
+		ny.left = node;
+		node.parent = ny;
+	}
 
-  function deleteFixup(node) {
-    var w;
-    while ((node != root) && (node.color == Rbt.Color.BLACK)) {
-      if (node == node.parent.left) {
-        w = node.parent.right;
-        if (w.color == Rbt.Color.RED) {
-          w.color = Rbt.Color.BLACK;
-          node.parent.color = Rbt.Color.RED;
-          leftRotate(node.parent);
-          w = node.parent.right;
-        }
-        if ((w.left.color == Rbt.Color.BLACK) && (w.right.color == Rbt.Color.BLACK)) {
-          w.color = Rbt.Color.RED;
-          node = node.parent;
-        } else {
-          if (w.right.color == Rbt.Color.BLACK) {
-            w.left.color = Rbt.Color.BLACK;
-            w.color = Rbt.Color.RED;
-            rightRotate(w);
-            w = node.parent.right;
-          }
-          w.color = node.parent.color;
-          node.parent.color = Rbt.Color.BLACK;
-          w.right.color = Rbt.Color.BLACK;
-          leftRotate(node.parent);
-          node = root;
-        }
-      } else {
-        w = node.parent.left;
-        if (w.color == Rbt.Color.RED) {
-          w.color = Rbt.Color.BLACK;
-          node.parent.color = Rbt.Color.RED;
-          rightRotate(node.parent);
-          w = node.parent.left;
-        }
-        if ((w.left.color == Rbt.Color.BLACK) && (w.right.color == Rbt.Color.BLACK)) {
-          w.color = Rbt.Color.RED;
-          node = node.parent;
-        } else {
-          if (w.left.color == Rbt.Color.BLACK) {
-            w.right.color = Rbt.Color.BLACK;
-            w.color = Rbt.Color.RED;
-            leftRotate(w);
-            w = node.parent.left;
-          }
-          w.color = node.parent.color;
-          node.parent.color = Rbt.Color.BLACK;
-          w.left.color = Rbt.Color.BLACK;
-          rightRotate(node.parent);
-          node = root;
-        }
-      }
-    }
-    node.color = Rbt.Color.BLACK;
-  }
+	function rightRotate(node) {
+		var ny = node.left;
+
+		node.left = ny.right;
+		if (ny.right != Rbt.NULL)
+			ny.right.parent = node;
+
+		ny.parent = node.parent;
+		if (node.parent == Rbt.NULL)
+			root = ny;
+		else if (node == node.parent.left)
+			node.parent.left = ny;
+		else
+			node.parent.right = ny;
+
+		ny.right = node;
+		node.parent = ny;
+	}
+
+	function insertFixup(node) {
+		var y;
+		while (node.parent.color == Rbt.Color.RED) {
+			if (node.parent == node.parent.parent.left) {
+				y = node.parent.parent.right;
+				if (y.color == Rbt.Color.RED) {
+					node.parent.color = Rbt.Color.BLACK;
+					y.color = Rbt.Color.BLACK;
+					node.parent.parent.color = Rbt.Color.RED;
+					node = node.parent.parent;
+				} else {
+					if (node == node.parent.right) {
+						node = node.parent;
+						leftRotate(node);
+					}
+					node.parent.color = Rbt.Color.BLACK;
+					node.parent.parent.color = Rbt.Color.RED;
+					rightRotate(node.parent.parent);
+				}
+			} else {
+				y = node.parent.parent.left;
+				if (y.color == Rbt.Color.RED) {
+					node.parent.color = Rbt.Color.BLACK;
+					y.color = Rbt.Color.BLACK;
+					node.parent.parent.color = Rbt.Color.RED;
+					node = node.parent.parent;
+				} else {
+					if (node == node.parent.left) {
+						node = node.parent;
+						rightRotate(node);
+					}
+					node.parent.color = Rbt.Color.BLACK;
+					node.parent.parent.color = Rbt.Color.RED;
+					leftRotate(node.parent.parent);
+				}
+			}
+		}
+		root.color = Rbt.Color.BLACK;
+	}
+
+	function deleteFixup(node) {
+		var w;
+		while ((node != root) && (node.color == Rbt.Color.BLACK)) {
+			if (node == node.parent.left) {
+				w = node.parent.right;
+				if (w.color == Rbt.Color.RED) {
+					w.color = Rbt.Color.BLACK;
+					node.parent.color = Rbt.Color.RED;
+					leftRotate(node.parent);
+					w = node.parent.right;
+				}
+				if ((w.left.color == Rbt.Color.BLACK)
+						&& (w.right.color == Rbt.Color.BLACK)) {
+					w.color = Rbt.Color.RED;
+					node = node.parent;
+				} else {
+					if (w.right.color == Rbt.Color.BLACK) {
+						w.left.color = Rbt.Color.BLACK;
+						w.color = Rbt.Color.RED;
+						rightRotate(w);
+						w = node.parent.right;
+					}
+					w.color = node.parent.color;
+					node.parent.color = Rbt.Color.BLACK;
+					w.right.color = Rbt.Color.BLACK;
+					leftRotate(node.parent);
+					node = root;
+				}
+			} else {
+				w = node.parent.left;
+				if (w.color == Rbt.Color.RED) {
+					w.color = Rbt.Color.BLACK;
+					node.parent.color = Rbt.Color.RED;
+					rightRotate(node.parent);
+					w = node.parent.left;
+				}
+				if ((w.left.color == Rbt.Color.BLACK)
+						&& (w.right.color == Rbt.Color.BLACK)) {
+					w.color = Rbt.Color.RED;
+					node = node.parent;
+				} else {
+					if (w.left.color == Rbt.Color.BLACK) {
+						w.right.color = Rbt.Color.BLACK;
+						w.color = Rbt.Color.RED;
+						leftRotate(w);
+						w = node.parent.left;
+					}
+					w.color = node.parent.color;
+					node.parent.color = Rbt.Color.BLACK;
+					w.left.color = Rbt.Color.BLACK;
+					rightRotate(node.parent);
+					node = root;
+				}
+			}
+		}
+		node.color = Rbt.Color.BLACK;
+	}
 };
